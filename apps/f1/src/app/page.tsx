fb7b0f01c93487e558be7585fd9414e4a830a0ef
sedna-ken <ken.takagi@sedna.com>
2023-03-05 00:35:57 -0800

    add turbo

---
+++ apps/f1/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+import { Button } from "ui";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <h1>F1</h1>
+      <Button />
+    </div>
+  );
+}
